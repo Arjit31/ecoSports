3227f7945417e590361839fdffe538767235eb69
arjit <apk20023110@gmail.com>
2024-10-13 17:40:15 +0530

    second

---
+++ models/user.js
@@ -0,0 +1,26 @@
+const mongodb = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/EcoSport')
+
+const userSchema = mongoose.userSchema({
+    name: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+})
+
+module.exports = mongoose.model('user', userSchema);
